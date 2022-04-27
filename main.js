410faf9a4ac982db4b7731a9a54b132da4fd790d
Daniel <43777012+DanielKaziev@users.noreply.github.com>
2022-04-27 22:21:22 +0600

    ~ 3/4 part of development.
    Added logic for calculating wins/losses
    Added soundtrack.
    Added display of messages about victory or defeat.
    Added the ability to replay the game.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function start(){
     var cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11];
     var dealer = [];
     var player = [];
-    // раздача карт для начала игры
+
     for (let i = 0; i < 2; i++) { 
         let num = Math.floor(Math.random() * cards.length);
         dealer.push(cards[num]);
@@ -13,17 +13,16 @@ function start(){
         player.push(cards[num]);
         cards.splice(num, 1);
     }
-    // рендер кнопок
     document.getElementById("take").style.display = "inline-block";
     document.getElementById("stand").style.display = "inline-block";
     document.getElementById("start").style.display = "none";
     
-    // добор карт
+
     take.onclick = function take(){
         let num = Math.floor(Math.random() * cards.length);
         player.push(cards[num]);
         cards.splice(num, 1);
-        // Если у  дилера менише 17 очков, то добирает.
+
         if (dealer.reduce((a,b)=>a+b) < 17){
             let num = Math.floor(Math.random() * cards.length);
             dealer.push(cards[num]);
@@ -31,10 +30,12 @@ function start(){
         }
         document.getElementById("dealer").innerHTML = dealer.reduce((a,b)=>a+b);
         document.getElementById("player").innerHTML = player.reduce((a,b)=>a+b);
+        if (dealer.reduce((a,b)=>a+b) >= 21 || player.reduce((a,b)=>a+b) >= 21){
+            checkWhoWon()
+        }
     }
-    // оставит карты
+
     stand.onclick = function take(){
-        // Если у  дилера менише 17 очков, то добирает.
         while(dealer.reduce((a,b)=>a+b) < 17){
             let num = Math.floor(Math.random() * cards.length);
             dealer.push(cards[num]);
@@ -43,16 +44,71 @@ function start(){
         }
         document.getElementById("dealer").innerHTML = dealer.reduce((a,b)=>a+b);
         document.getElementById("player").innerHTML = player.reduce((a,b)=>a+b);
+        checkWhoWon();
     }
     
     function checkWhoWon(){
         let dealer_op = dealer.reduce((a,b)=>a+b);
         let player_op = player.reduce((a,b)=>a+b);
-    }
-    console.log(player);
-    console.log(dealer);
-    console.log(cards);
 
+        document.getElementById("text").style.display = "inline-block";
+        document.getElementById("reset").style.display = "block";
+        document.getElementById("take").style.display = "none";
+        document.getElementById("stand").style.display = "none";
+
+
+        if (player_op == dealer_op){
+            document.getElementById("text").innerHTML = "Ничья";
+            drawSound();
+        } else if (dealer_op == 21) {
+            document.getElementById("text").innerHTML = "Дилер выиграл";
+            loseSound();
+        } else if (player_op == 21) {
+            document.getElementById("text").innerHTML = "Игрок выиграл";
+            winSound();
+        } else if (dealer_op > player_op){
+            if (dealer_op < 21){
+                document.getElementById("text").innerHTML = "Дилер выиграл";
+                loseSound();
+            } else if(dealer_op > 21){
+                document.getElementById("text").innerHTML = "Игрок выиграл";
+                winSound();
+            }
+        } else if (player_op > dealer_op){
+            if (player_op < 21){
+                document.getElementById("text").innerHTML = "Игрок выиграл";
+                winSound();
+            } else if(player_op > 21){
+                document.getElementById("text").innerHTML = "Дилер выиграл";
+                loseSound();
+            }
+        }
+    }
     document.getElementById("dealer").innerHTML = dealer.reduce((a,b)=>a+b);
     document.getElementById("player").innerHTML = player.reduce((a,b)=>a+b);
+    
+    document.getElementById("reset").onclick = function reset(){
+        document.getElementById("text").style.display = "none";
+        document.getElementById("reset").style.display = "none";
+        document.getElementById("take").style.display = "none";
+        document.getElementById("stand").style.display = "none";
+        document.getElementById("start").style.display = "block";
+        document.getElementById("dealer").innerHTML = "0";
+        document.getElementById("player").innerHTML = "0";
+    }
+    function winSound() {
+        var audio = new Audio(); 
+        audio.src = "win.mp3";
+        audio.autoplay = true;
+    }
+    function loseSound() {
+        var audio = new Audio(); 
+        audio.src = "lose.mp3";
+        audio.autoplay = true;
+    }
+    function drawSound() {
+        var audio = new Audio(); 
+        audio.src = "draw.mp3";
+        audio.autoplay = true;
+    }
 }
